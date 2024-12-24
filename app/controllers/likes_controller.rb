94541b66435a73e646def7ca5b2d0d7d97e9dbfe
msteq <msteqrun@gmail.com>
2024-12-25 01:07:40 +0900

    いいね機能実装

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,40 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+
+  def create
+    @like = @post.likes.build(user: current_user)
+    if @like.save
+      respond_to do |format|
+        format.html { redirect_to @post, notice: 'いいねしました。' }
+        format.turbo_stream
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @post, alert: 'いいねに失敗しました。' }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("like_button_#{@post.id}", partial: "posts/like_button", locals: { post: @post }) }
+      end
+    end
+  end
+
+  def destroy
+    @like = @post.likes.find_by(user: current_user)
+    if @like&.destroy
+      respond_to do |format|
+        format.html { redirect_to @post, notice: 'いいねを解除しました。' }
+        format.turbo_stream
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @post, alert: 'いいねの解除に失敗しました。' }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("like_button_#{@post.id}", partial: "posts/like_button", locals: { post: @post }) }
+      end
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_02_055643) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_19_083306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_02_055643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_habits_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -70,6 +80,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_02_055643) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "habits", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "habits"
   add_foreign_key "posts", "users"
   add_foreign_key "progresses", "habits"
