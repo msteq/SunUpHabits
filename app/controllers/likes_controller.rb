5cc3e7e628aa6cb2f0659ca1aa9c5757305ad47d
msteq <msteqrun@gmail.com>
2024-12-25 01:12:28 +0900

    いいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,12 +6,12 @@ class LikesController < ApplicationController
     @like = @post.likes.build(user: current_user)
     if @like.save
       respond_to do |format|
-        format.html { redirect_to @post, notice: 'いいねしました。' }
+        format.html { redirect_to @post, notice: "いいねしました。" }
         format.turbo_stream
       end
     else
       respond_to do |format|
-        format.html { redirect_to @post, alert: 'いいねに失敗しました。' }
+        format.html { redirect_to @post, alert: "いいねに失敗しました。" }
         format.turbo_stream { render turbo_stream: turbo_stream.replace("like_button_#{@post.id}", partial: "posts/like_button", locals: { post: @post }) }
       end
     end
@@ -21,12 +21,12 @@ class LikesController < ApplicationController
     @like = @post.likes.find_by(user: current_user)
     if @like&.destroy
       respond_to do |format|
-        format.html { redirect_to @post, notice: 'いいねを解除しました。' }
+        format.html { redirect_to @post, notice: "いいねを解除しました。" }
         format.turbo_stream
       end
     else
       respond_to do |format|
-        format.html { redirect_to @post, alert: 'いいねの解除に失敗しました。' }
+        format.html { redirect_to @post, alert: "いいねの解除に失敗しました。" }
         format.turbo_stream { render turbo_stream: turbo_stream.replace("like_button_#{@post.id}", partial: "posts/like_button", locals: { post: @post }) }
       end
     end
