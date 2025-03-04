d6c70f0410ea0d7b28770952993b673839b55309
msteq <msteqrun@gmail.com>
2025-03-04 20:48:12 +0900

    パスワード再設定機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   # ルートパスの設定
   root "static_pages#top"
 
   # Deviseのルーティングをカスタムコントローラーで設定
   devise_for :users, controllers: {
     registrations: "users/registrations",
-    sessions: "users/sessions"
+    sessions: "users/sessions",
+    passwords: "users/passwords"
   }
 
   # マイページ画面へのルートを設定
