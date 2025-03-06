33e20cee9462b66b4dd95a90f62273c6d8d185bc
msteq <msteqrun@gmail.com>
2025-03-06 17:48:51 +0900

    利用規約実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  # ルートパスの設定
   root "static_pages#top"
 
-  # Deviseのルーティングをカスタムコントローラーで設定
+  get "terms", to: "static_pages#terms", as: :terms
+
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
     passwords: "users/passwords"
   }
 
-  # マイページ画面へのルートを設定
   get "my_page", to: "users#show", as: :my_page
 
-  # Habitsのルーティング
   resources :habits do
     member do
       post "achieve"
@@ -22,10 +20,8 @@ Rails.application.routes.draw do
     end
   end
 
-  # マイ習慣画面へのルートを設定
   get "my_habits", to: "habits#index", as: "my_habits"
 
-  # Postsのルーティング
   resources :posts, only: [ :index, :show, :new, :create ] do
     resources :comments, only: [ :create, :destroy ]
     resources :likes, only: [ :create, :destroy ]
